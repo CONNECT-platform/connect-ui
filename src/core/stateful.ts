5bad7debd0e8e1a8f8d61f02757cb7a59542720a
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-01-31 12:22:48 +0100

    added properties to statefuls

--- src/core/stateful.ts
+++ src/core/stateful.ts
@@ -1,35 +1,53 @@
 import { Agent } from './base/agent';
 import { Signature } from './base/signature';
+import { InputPin, OutputPin, PersistentOutput } from './base/io';
 import { State } from './state';
 
 
 export interface StatefulSignature extends Signature {
-  states: string[];
+  states?: string[];
+  properties?: string[];
 }
 
 export class Stateful extends Agent {
-  private states: {[name: string]: State<any>} = {};
+  private states: {[name: string]: State<any>};
 
-  constructor(signature: StatefulSignature) {
-    super({
-      inputs: (signature.inputs || []).concat(signature.states),
-      outputs: (signature.outputs || []).concat(signature.states),
-      signals: signature.signals,
-    });
-
-    signature.states.forEach(state => {
-      let _state = this.createState(state);
-      let input = this.inputs.get(state);
-      let output = this.outputs.get(state);
-
-      this.states[state] = _state;
-
-      input.onReceived.subscribe(data => _state.in.receive(data));
-      _state.out.onSent.subscribe(data => output.send(data));
-    });
+  constructor(readonly signature: StatefulSignature) {
+    super(Object.assign(signature, {
+      inputs: (signature.inputs || []).concat(signature.states || []),
+      outputs: (signature.outputs || []).concat(signature.states || []).concat(signature.properties || []),
+    }));
   }
 
   public state(state: string): State<any> { return this.states[state]; }
+  public property(property: string): PersistentOutput<any> {
+    if (this.signature.properties && this.signature.properties.includes(property))
+      return this.outputs.get(property);
+    return undefined;
+  }
 
-  protected createState(state: String): State<any> { return new State(); }
+  protected createState(state: string): State<any> { return new State(); }
+
+  protected preBuild() {
+    this.states = {};
+
+    if (this.signature.states)
+      this.signature.states.forEach(state => {
+        this.states[state] = this.createState(state);
+      });
+  }
+
+  protected createInput(input: string): InputPin<any> {
+    if (this.signature.states && this.signature.states.includes(input))
+      return this.states[input].in;
+    else return super.createInput(input);
+  }
+
+  protected createOutput(output: string): OutputPin<any> | PersistentOutput<any> {
+    if (this.signature.properties && this.signature.properties.includes(output))
+      return new PersistentOutput<any>();
+    else if (this.signature.states && this.signature.states.includes(output))
+      return this.states[output].out;
+    else return super.createOutput(output);
+  }
 }
--- src/core/test/stateful.ts
+++ src/core/test/stateful.ts
@@ -2,6 +2,7 @@ import { should, expect } from 'chai'; should();
 
 import { Stateful } from '../stateful';
 import { State } from '../state';
+import { PersistentOutput } from '../base/io';
 
 
 describe('Stateful', () => {
@@ -9,6 +10,11 @@ describe('Stateful', () => {
     let s = new Stateful({ states: ['a', 'b'] });
     s.inputs.has('a').should.be.true;
     s.outputs.has('b').should.be.true;
+  });
+
+  it('should create `PersistentOutput`s for its properties.', () => {
+    let s = new Stateful({ properties: ['x'] });
+    expect(s.outputs.get('x')).to.be.instanceof(PersistentOutput);
   });
 
   describe('.state()', () => {
@@ -22,6 +28,18 @@ describe('Stateful', () => {
       let s = new Stateful({states: ['a']});
       s.state('a').out.send(2);
       s.outputs.get('a').last.should.equal(2);
+    });
+  });
+
+  describe('.property', () => {
+    it('should return the output pin corresponding to a property.', () => {
+      let s = new Stateful({properties: ['a']});
+      s.property('a').should.equal(s.outputs.get('a'));
+    });
+
+    it('should return `undefined` for unspecified properties.', () => {
+      let s = new Stateful({properties: ['a'], outputs: ['b']});
+      expect(s.property('b')).to.be.undefined;
     });
   });
 
